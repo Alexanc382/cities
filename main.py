8d2b9af40e7ec3d1bc37a8f081ba45ad05d95e11
Alexanc382 <alexanc382@gmail.com>
2025-08-01 09:49:44 +0300

    добавила комментарии к коду

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from opencage.geocoder import OpenCageGeocode
 
 
-def get_coordinates(city, key): # передадим в фунцию аргументы из city и key
+def get_coordinates(city, key): # передадим в функцию аргументы из city и key
     try:
-        geocoder = OpenCageGeocode(key) # создаёт объект geocoder для работы с API OpenCage Geocoding на основе переданного API-ключа key
-        results = geocoder.geocode(city, language='ru')
-        if results: # если всё нормально geocoder вернется в переменную results
-            return results[0]['geometry']['lat'], results[0]['geometry']['lng'] # функция get_... возвращает нам координаты (55.625578, 37.6063916)
-        # lat - широта, lng - долгота
+        geocoder = OpenCageGeocode(key) # создаёт объект geocoder для работы с API OpenCage Geocoding на основе переданного API-ключа 'key'
+        results = geocoder.geocode(city, language='ru') # запрос к сервису с передачей 'city', получение данных (координат) с сервера, результат — в results.
+        if results: # если всё нормально координаты вернутся в переменную results и запустится if/else
+            return results[0]['geometry']['lat'], results[0]['geometry']['lng'] # функция get_... возвращает нам координаты (55.625578, 37.6063916) по 'пути',
+            # который мы прописали (см. путь и индексы в файле json с сайта OpenCage)
+            # lat - широта, lng - долгота
         else:
             return 'Город не найден'
     except Exception as e:
