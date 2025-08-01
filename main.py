27cece98bd999593d28ef82f74a66b824aeb8790
Alexanc382 <alexanc382@gmail.com>
2025-08-01 09:39:43 +0300

    возвращаем результат на русском

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from opencage.geocoder import OpenCageGeocode
 def get_coordinates(city, key): # передадим в фунцию аргументы из city и key
     try:
         geocoder = OpenCageGeocode(key) # создаёт объект geocoder для работы с API OpenCage Geocoding на основе переданного API-ключа key
-        query = city
-        results = geocoder.geocode(query)  #
+        results = geocoder.geocode(city, language='ru')
         if results: # если всё нормально geocoder вернется в переменную results
             return results[0]['geometry']['lat'], results[0]['geometry']['lng'] # функция get_... возвращает нам координаты (55.625578, 37.6063916)
         # lat - широта, lng - долгота
