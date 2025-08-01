4b77b76c9d9a432ca40612c546eefea027cb26ba
Alexanc382 <alexanc382@gmail.com>
2025-08-01 09:56:05 +0300

    добавлен файл со сл. версией программы, где объявлены две новое переменные lat и lng и помешены туда значения из return results

---
+++ main_vers_1.1.py
@@ -0,0 +1,22 @@
+from opencage.geocoder import OpenCageGeocode
+
+
+def get_coordinates(city, key): # передадим в функцию аргументы из city и key
+    try:
+        geocoder = OpenCageGeocode(key) # создаёт объект geocoder для работы с API OpenCage Geocoding на основе переданного API-ключа 'key'
+        results = geocoder.geocode(city, language='ru') # запрос к сервису с передачей 'city', получение данных (координат) с сервера, результат — в results.
+        if results: # если всё нормально координаты вернутся в переменную results и запустится if/else
+        # lat - широта, lng - долгота
+            var_lat = results[0]['geometry']['lat']
+            var_lng = results[0]['geometry']['lng']
+            return  # функция get_... возвращает нам координаты (55.625578, 37.6063916) по 'пути',
+            # который мы прописали (см. путь и индексы в файле json с сайта OpenCage)
+        else:
+            return 'Город не найден'
+    except Exception as e:
+        return f'Возникла ошибка: {e}'
+
+key = 'b129484e1f8f49dc8c1625f078a65dc2'
+city = 'London' # указываем город, координаты которого хотим запросить
+coordinates = get_coordinates(city, key)
+print(f'Координаты города {city}: {coordinates}')
