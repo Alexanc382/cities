028cb5774f135e490884de037225d837e101eeb3
Alexanc382 <alexanc382@gmail.com>
2025-08-01 09:37:00 +0300

    изменен город

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ from opencage.geocoder import OpenCageGeocode
 
 
 def get_coordinates(city, key): # передадим в фунцию аргументы из city и key
-    geocoder = OpenCageGeocode(key) # создаёт объект geocoder для работы с API OpenCage Geocoding на основе переданного API-ключа key
-    query = city
-    results = geocoder.geocode(query)  #
-    if results: # если всё нормально geocoder вернется в переменную results
-        return results[0]['geometry']['lat'], results[0]['geometry']['lng'] # функция get_... возвращает нам координаты (55.625578, 37.6063916)
-    # lat - широта, lng - долгота
-    else:
-        return 'Город не найден'
-
+    try:
+        geocoder = OpenCageGeocode(key) # создаёт объект geocoder для работы с API OpenCage Geocoding на основе переданного API-ключа key
+        query = city
+        results = geocoder.geocode(query)  #
+        if results: # если всё нормально geocoder вернется в переменную results
+            return results[0]['geometry']['lat'], results[0]['geometry']['lng'] # функция get_... возвращает нам координаты (55.625578, 37.6063916)
+        # lat - широта, lng - долгота
+        else:
+            return 'Город не найден'
+    except Exception as e:
+        return f'Возникла ошибка: {e}'
 
 key = 'b129484e1f8f49dc8c1625f078a65dc2'
-city = 'Moscow' # указываем город, координаты которого хотим запросить
+city = 'London' # указываем город, координаты которого хотим запросить
 coordinates = get_coordinates(city, key)
 print(f'Координаты города {city}: {coordinates}')
